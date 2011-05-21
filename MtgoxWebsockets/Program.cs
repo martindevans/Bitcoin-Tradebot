d97c8182650c7e547ea99a045ff08ae939ef57f2
Martin Evans <martindevans@gmail.com>
2011-05-21 21:10:18 +0100

    Written a strategy tester, which runs a dataset of the days trading against a set of strategies and returns them in ranked order of profit
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- MtgoxWebsockets/Program.cs
+++ MtgoxWebsockets/Program.cs
@@ -17,6 +17,38 @@ namespace MtgoxWebsockets
     {
         static void Main(string[] args)
         {
+            //ListenToMtgox();
+            RunFakeTests();
+        }
+
+        private static void RunFakeTests()
+        {
+            var data = new string[]
+            {
+                "Training/0.t"
+            }.Select(a => File.ReadAllLines(a));
+
+            StrategyTester t = new StrategyTester(data, new Func<TradeStrategy>[]
+            {
+                //Add new type of strategy to this array as a factory method to construct the strategy
+                () => new Stupid()
+            });
+
+            var result = t.Run(0, 10).ToArray();
+
+            Console.Clear();
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Value.Name + " with $" + item.Key);
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+
+        private static void ListenToMtgox()
+        {
             var ws = new WebSocket("ws://" + Dns.GetHostAddresses("websocket.mtgox.com").First().ToString() + ":80/mtgox", "*");
             var observableWs = new ObservableWebsocket(ws);
 
@@ -49,36 +81,39 @@ namespace MtgoxWebsockets
                 //fakeDepth;
                 data.AsJson().AsMtgoxDepth();
             IObservable<Trade> trade = 
-                //fakeTrades;
-                data.AsJson().AsMtgoxTrades();
+                fakeTrades;
+                //data.AsJson().AsMtgoxTrades();
 
             TradeManager manager = new TradeManager(exchange, ticker, depth, trade, new Stupid());
 
-            var tradelog = new StreamWriter(File.Open("trades.t", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read));
-            manager.Trades.Subscribe(a => tradelog.WriteLine(String.Format("tr {0} {1}", a.Volume, a.Price)));
+            //var tradelog = new StreamWriter(File.Open("trades.t", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read));
+            //manager.Trades.Subscribe(a => tradelog.WriteLine(String.Format("tr {0} {1}", a.Volume, a.Price)));
 
             manager.Trades.Subscribe(a => Console.WriteLine(String.Format("Mtgox Traded {0} BTC for {1} USD", a.Volume, a.Price)));
             //manager.Ticker.Subscribe(a => Console.Title = a.ToString());
 
-            //while (true)
-            //{
-            //    Console.WriteLine("Enter a line to inject fake data");
-            //    Console.WriteLine("tr {volume} {price}");
+            var trFile = new StreamReader("Training/0.t");
 
-            //    var line = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Enter a line to inject fake data");
+                Console.WriteLine("tr {volume} {price}");
 
-            //    var tokens = line.Split(' ').Select(a => a.ToLower()).ToArray();
-            //    if (tokens[0] == "tr" && tokens.Length == 3)
-            //    {
-            //        fakeTrades.OnNext(new Trade(decimal.Parse(tokens[1]), decimal.Parse(tokens[2]), DateTime.Now.ToUniversalTime().Ticks));
-            //    }
-            //}
+                var line = Console.ReadLine();
+                line = trFile.ReadLine();
+
+                var tokens = line.Split(' ').Select(a => a.ToLower()).ToArray();
+                if (tokens[0] == "tr" && tokens.Length == 3)
+                {
+                    fakeTrades.OnNext(new Trade(decimal.Parse(tokens[1]), decimal.Parse(tokens[2]), DateTime.Now.ToUniversalTime().Ticks));
+                }
+            }
 
             Console.WriteLine("Press any key to exit");
             Console.Read();
 
-            tradelog.Flush();
-            tradelog.Close();
+            //tradelog.Flush();
+            //tradelog.Close();
         }
     }
 }
---
+++ MtgoxWebsockets/StrategyTester.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MtgoxWebsockets.Exchanges;
+
+namespace MtgoxWebsockets
+{
+    public class StrategyTester
+    {
+        public readonly IEnumerable<IEnumerable<string>> TestData;
+        public readonly Func<TradeStrategy>[] Strategies;
+
+        public StrategyTester(IEnumerable<IEnumerable<string>> testDataSets, IEnumerable<Func<TradeStrategy>> strategies)
+        {
+            TestData = testDataSets;
+            Strategies = strategies.ToArray();
+        }
+
+        public IEnumerable<KeyValuePair<decimal, Type>> Run(decimal startingBtc, decimal startingCurrency)
+        {
+            return Strategies
+                .SelectMany(s => TestData.Select(d => new { Strategy = s(), Data = d.Select(a => ParseString(a)), Exchange = new FakeExchange(startingCurrency, startingBtc) }))
+                .Select(a => new { Params = a, Manager = new TradeManager(a.Exchange, new Ticker[0].ToObservable(), new DepthTable.Entry[0].ToObservable(), a.Data.ToObservable(), new[] { a.Strategy }) })
+                .Select(a => new { Currency = a.Params.Exchange.Btc * a.Params.Data.Last().Price + a.Params.Exchange.Currency, Strategy = a.Params.Strategy.GetType() })
+                .OrderBy(a => a.Currency)
+                .Select(a => new KeyValuePair<decimal, Type>(a.Currency, a.Strategy));
+        }
+
+        private Trade ParseString(string line)
+        {
+            var tokens = line.Split(' ').Select(a => a.ToLower()).ToArray();
+            if (tokens[0] == "tr" && tokens.Length == 3)
+                return new Trade(decimal.Parse(tokens[1]), decimal.Parse(tokens[2]), DateTime.Now.ToUniversalTime().Ticks);
+
+            throw new ArgumentException("Invalid string");
+        }
+    }
+}
